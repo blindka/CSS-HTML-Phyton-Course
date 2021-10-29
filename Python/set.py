db7c8fd2be1519d727239f1c425572eda89a68e8
Eyal Haimov <eyalhaimoon@gmail.com>
2021-10-29 12:20:06 +0300

    Add files via upload

---
+++ Python/set.py
@@ -0,0 +1,13 @@
+# Create an empty set
+s = set()
+
+# Add elements to set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(3)
+s.add(6)
+s.remove(6)
+print(s)
+print(f"The set has {len(s)} elements")
